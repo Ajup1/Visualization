2b9e7b6ec247eab54df97c6fd977a828462a00e1
Ajup1 <pujabhandari01@gmail.com>
2021-07-24 10:33:13 -0500

    New File

--- d3.parcoords.js
+++ d3.parcoords.js
@@ -693,7 +693,7 @@ pc.clear = function(layer) {
 
   // This will make sure that the foreground items are transparent
   // without the need for changing the opacity style of the foreground canvas
-  // as this would stop the css styling from working
+  // as this would stop ts styling from working
   if(layer === "brushed" && isBrushed()) {
     ctx.brushed.fillStyle = pc.selection.style("background-color");
     ctx.brushed.globalAlpha = 1 - __.alphaOnBrushed;
